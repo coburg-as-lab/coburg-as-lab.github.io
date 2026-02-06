0900220b3b08719096760843709a36013769ba84
look-andreas <look.andreas90@gmail.com>
2026-02-06 20:56:06 +0000

    Deploying to gh-pages from @ coburg-as-lab/coburg-as-lab.github.io@7974a0fcc091896bf4ba3bf95ac08dea5749dd78 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -476,7 +476,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=3A6LKtAAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
